e5ee39a431d6923fa146f328a035f4df7d676c57
CHOI JUN YOUNG <93821377+CJY512@users.noreply.github.com>
2024-04-17 10:57:43 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240417/체크판위에서 2/on-the-checkboard-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int r, c;
+char field[20][20];
+int answer;
+
+void bt(int cur_r, int cur_c, int jump_cnt) {
+    if(jump_cnt == 3) {
+        answer++;
+        return;
+    }
+
+    char cur_color = field[cur_r][cur_c];
+    for(int i{cur_r + 1}; i < r; i++) {
+        for(int j{cur_c + 1}; j < c; j++) {
+            if(field[i][j] == cur_color) continue;
+
+            bt(i, j, jump_cnt + 1);
+        }
+    }
+}
+
+int main() {
+
+    cin >> r >> c;
+    for(int i{0};i < r; i++) {
+        for(int j{0};j < c;j++) {
+            cin >> field[i][j];
+        }
+    }
+
+    bt(0, 0, 0);
+    cout << answer;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
