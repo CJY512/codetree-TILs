b76f3baf183a52c37b6c3150b26b1617f64c3c64
CHOI JUN YOUNG <93821377+CJY512@users.noreply.github.com>
2024-05-23 09:41:34 +0900

    Title: 아름다운 수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240523/아름다운 수/beautiful-number.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int n;
+
+void bt(int rest, int& result) {
+    if (rest == 0) {
+        result++;
+        return;
+    }
+
+    for (int i{ 1 }; i <= n; i++) {
+        if (rest < i) continue;
+        bt(rest - i, result);
+    }
+}
+
+int main() {
+
+    std::cin >> n;
+
+    int ans;
+    bt(n, ans);
+
+    std::cout << ans;
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
