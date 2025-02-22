a92f6b15faeb2e575c0d1ece6ff7d5860e308782
CHOI JUN YOUNG <93821377+CJY512@users.noreply.github.com>
2025-02-22 15:05:26 +0900

    Title: 정수 명령 처리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250222/정수 명령 처리/process-numeric-commands.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int n; std::cin >> n;
+    std::string cmd;
+    int arg;
+    std::stack<int> stk;
+    while(n--) {
+        std::cin >> cmd;
+        if (cmd == "push") {
+            std::cin >> arg;
+            stk.push(arg);
+        } else if (cmd == "size") {
+            std::cout << stk.size() << "\n";
+        } else if (cmd == "empty") {
+            std::cout << stk.empty() << "\n";
+        } else if (cmd == "pop") {
+            arg = stk.top();
+            std::cout << arg << "\n";
+            stk.pop();
+        } else {
+            std::cout << "cannot reach here!\n";
+        }
+    }
+    return 0;
+}
